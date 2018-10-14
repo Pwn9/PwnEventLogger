6c00c82db9588ff5afaa2a4a34cb33a3182a6129
msudol <matt.sudol@gmail.com>
2018-10-14 11:24:09 -0400

    things work better when you don't make typos

--- src/com/pwn9/PwnEventLogger/Config.java
+++ src/com/pwn9/PwnEventLogger/Config.java
@@ -5,7 +5,7 @@ public class Config
 	public static void LoadConfig() 
 	{
 		PwnEventLogger.BlockGrowEvent = PwnEventLogger.instance.getConfig().getBoolean("BlockGrowEvent", false);
-		PwnEventLogger.BlockSpreadEvent = PwnEventLogger.instance.getConfig().getBoolean("BlockSpreakEvent", false);
+		PwnEventLogger.BlockSpreadEvent = PwnEventLogger.instance.getConfig().getBoolean("BlockSpreadEvent", false);
 		PwnEventLogger.StructureGrowEvent = PwnEventLogger.instance.getConfig().getBoolean("StructureGrowEvent", false);
 		
 	}
